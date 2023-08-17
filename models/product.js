bfb34481fcf16100fdd28b61e9ea91234671eeea
sachindimri788 <sachindimri788@gmail.com>
2023-08-17 10:10:19 +0530

    Integrating CRUD with mongoDB

--- models/product.js
+++ models/product.js
@@ -1,19 +1,66 @@
 const getDB = require("../util/database").getDB;
-
+const mongodb = require("mongodb");
 class Product {
-  constructor(title, price, description, imageURl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
-    this.imageURl = imageURl;
+    this.imageUrl = imageUrl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
   save() {
     const db = getDB();
-    return db
-      .collection("products")
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      //Update the product
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOp = db.collection("products").insertOne(this);
+    }
+    return dbOp
       .then((result) => {
         console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static fetchAll() {
+    const db = getDB();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  static findById(prodId) {
+    const db = getDB();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static deleteById(prodId) {
+    const db = getDB();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((product) => {
+        console.log("deleted");
       })
       .catch((err) => {
         console.log(err);
