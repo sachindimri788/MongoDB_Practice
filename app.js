d1162a5ba5ff6bbeb83f99ee63b90cced61fa4e3
sachindimri788 <sachindimri788@gmail.com>
2023-08-17 10:18:06 +0530

    Relations between tables

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
   User.findById("642811cdaa73d64dd9937a98").then((user) => {
-    req.user = user;
+    req.user = new User(user.name, user.email, user.cart, user._id);
     next();
   });
 });
--- models/user.js
+++ models/user.js
@@ -1,10 +1,15 @@
 const getDB = require("../util/database").getDB;
 const mongodb = require("mongodb");
 
+const ObjectId = mongodb.ObjectId;
+
 const db = require("../util/database").getDB;
 class User {
-  constructor(username, email) {
-    (this.name = username), (this.email = email);
+  constructor(username, email, cart, id) {
+    this.name = username;
+    this.email = email;
+    this.cart = cart; //{items : []}
+    this._id = id;
   }
 
   save() {
@@ -22,6 +27,52 @@ class User {
       })
       .catch((err) => {
         console.log(err);
+      });
+  }
+
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex((cp) => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity,
+      });
+    }
+    const updatedCart = { items: updatedCartItems };
+    const db = getDB();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
+  }
+
+  getCart() {
+    const db = getDB();
+    const productIds = this.cart.items.map((i) => {
+      return i.productId;
+    });
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((p) => {
+          return {
+            ...p,
+            quantity: this.cart.items.find((i) => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity,
+          };
+        });
       });
   }
 }
